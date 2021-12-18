7fc4cc100f68e175f746d5d98a0328e85782ec46
CodingJunction <95361536+CodingJunction@users.noreply.github.com>
2021-12-18 20:59:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print("""Welcome
+This is a Band Name Generator
+To generate a name for your Band answer the given questions""")
+city = input('What is the name of your favourite city?\n').capitalize()
+wanimal = input('What is the name of your favourite wild animal?\n').capitalize()
+print("Your suggested Band name is",city,wanimal)
+
